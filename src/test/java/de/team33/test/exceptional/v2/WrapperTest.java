63f61a053deba216c850da4945d118fb5174b57f
andi <akk-pg@team33.de>
2019-02-03 16:58:02 +0100

    added some tests

--- src/test/java/de/team33/test/exceptional/v2/WrapperTest.java
+++ src/test/java/de/team33/test/exceptional/v2/WrapperTest.java
@@ -26,22 +26,8 @@ public class WrapperTest {
         }
     }
 
-    @Test(expected = IOException.class)
-    public void runConsumerFail() throws IOException {
-        EXPECT_IOX.run(() -> Stream.of("abc", "def", "ghi").forEach(Wrapper.consumer(str -> {
-            throw new IOException();
-        })));
-    }
-
-    @Test(expected = IOException.class)
-    public void getFunctionFail() throws IOException {
-        EXPECT_IOX.get(() -> Stream.of("abc", "def", "ghi").map(Wrapper.function(str -> {
-            throw new IOException();
-        })).collect(Collectors.toList()));
-    }
-
     @Test
-    public void runnableFail() {
+    public void runnable() {
         try {
             Wrapper.runnable(() -> tryString(false)).run();
             fail("expected to fail but worked");
@@ -51,14 +37,19 @@ public class WrapperTest {
     }
 
     @Test
-    public void runnableSuccess() {
-        Wrapper.runnable(() -> tryString(true)).run();
-    }
-
-    @Test
     public void consumer() {
         try {
             Wrapper.consumer(t -> tryString(false)).accept(null);
+            fail("expected to fail but worked");
+        } catch (WrappedException e) {
+            assertEquals(IOException.class, e.getCause().getClass());
+        }
+    }
+
+    @Test
+    public void biConsumer() {
+        try {
+            Wrapper.biConsumer((t, u) -> tryString(false)).accept(null, null);
             fail("expected to fail but worked");
         } catch (WrappedException e) {
             assertEquals(IOException.class, e.getCause().getClass());
@@ -79,6 +70,16 @@ public class WrapperTest {
     public void function() {
         try {
             final String result = Wrapper.function(t -> tryString(false)).apply(null);
+            fail("expected to fail but was " + result);
+        } catch (WrappedException e) {
+            assertEquals(IOException.class, e.getCause().getClass());
+        }
+    }
+
+    @Test
+    public void biFunction() {
+        try {
+            final String result = Wrapper.biFunction((t, u) -> tryString(false)).apply(null, null);
             fail("expected to fail but was " + result);
         } catch (WrappedException e) {
             assertEquals(IOException.class, e.getCause().getClass());
