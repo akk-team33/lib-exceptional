f51220ce9737cab7e637bcfabba3e9e3deb0c5ec
andi <akk-pg@team33.de>
2021-01-13 15:57:00 +0100

    new: Handling (v4, replacement of Insight, v3)

---
+++ src/main/java/de/team33/libs/exceptional/v4/Handling.java
@@ -0,0 +1,98 @@
+package de.team33.libs.exceptional.v4;
+
+import java.util.function.Function;
+
+/**
+ * A tool that supports the differentiated handling of exceptions.
+ */
+public class Handling<T extends Throwable> {
+
+    private final T subject;
+    private final Throwable cause;
+
+    private Handling(final T subject) {
+        this.subject = subject;
+        this.cause = subject.getCause();
+    }
+
+    /**
+     * Initiates a {@link Handling} of the given {@code subject}.
+     */
+    public static <T extends Throwable> Handling<T> of(final T subject) {
+        return new Handling<>(subject);
+    }
+
+    private static <X extends Throwable> void throwIfPresent(final X exception) throws X {
+        if (null != exception) {
+            throw exception;
+        }
+    }
+
+    /**
+     * <p>Applies the given {@code mapping} to the {@linkplain #of(Throwable) associated subject} and throws the result
+     * if is it NOT {@code null}. Otherwise this {@link Handling} will be returned.</p>
+     *
+     * <p>This method is used to re-specify an exception. For example, if an exception was caught as a very common
+     * type, e.g. an {@link Exception}. At the same time, a suitable mapping to the respective exception can be
+     * applied with this variant.</p>
+     *
+     * @see #reThrowIf(Class)
+     */
+    public final <X extends Throwable> Handling<T> throwMapped(final Function<? super T, X> mapping) throws X {
+        throwIfPresent(mapping.apply(subject));
+        return this;
+    }
+
+    /**
+     * <p>Re-throws the {@linkplain #of(Throwable) associated subject} if it matches the given exception {@code type}.
+     * Otherwise this {@link Handling} will be returned.</p>
+     *
+     * <p>This method is used to re-specify an exception. For example, if an exception was caught as a very common
+     * type, e.g. an {@link Exception}.</p>
+     *
+     * @see #throwMapped(Function)
+     */
+    public final <X extends Throwable> Handling<T> reThrowIf(final Class<X> type) throws X {
+        throwIfPresent(type.isInstance(subject) ? type.cast(subject) : null);
+        return this;
+    }
+
+    /**
+     * <p>Applies the given {@code mapping} to the {@link Throwable#getCause() cause} of the
+     * {@linkplain #of(Throwable) associated subject} and throws the result if is it NOT {@code null}.
+     * Otherwise this {@link Handling} will be returned.</p>
+     *
+     * <p>This method is used to bring the cause of an exception back to the foreground. For example,
+     * if an exception was previously caught and encapsulated in another exception for technical reasons,
+     * e.g. a {@link RuntimeException}. At the same time, a suitable mapping to the respective exception can be
+     * applied with this variant.</p>
+     *
+     * @see #reThrowCauseIf(Class)
+     */
+    public final <X extends Throwable> Handling<T> throwMappedCause(final Function<Throwable, X> mapping) throws X {
+        throwIfPresent(mapping.apply(cause));
+        return this;
+    }
+
+    /**
+     * <p>Re-throws the {@link Throwable#getCause() cause} of the {@linkplain #of(Throwable) associated subject}
+     * if it matches the given exception {@code type}. Otherwise this {@link Handling} will be returned.</p>
+     *
+     * <p>This method is used to bring the cause of an exception back to the foreground. For example,
+     * if an exception was previously caught and encapsulated in another exception for technical reasons,
+     * e.g. a {@link RuntimeException}.</p>
+     *
+     * @see #throwMappedCause(Function)
+     */
+    public final <X extends Throwable> Handling<T> reThrowCauseIf(final Class<X> type) throws X {
+        throwIfPresent(type.isInstance(cause) ? type.cast(cause) : null);
+        return this;
+    }
+
+    /**
+     * Returns the associated {@code subject}.
+     */
+    public final T fallback() {
+        return subject;
+    }
+}
---
+++ src/test/java/de/team33/test/exceptional/v4/HandlingTest.java
@@ -0,0 +1,200 @@
+package de.team33.test.exceptional.v4;
+
+import de.team33.libs.exceptional.v4.Handling;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+
+@SuppressWarnings({"MultipleTopLevelClassesInFile", "NestedTryStatement"})
+public class HandlingTest {
+
+    private static <X extends Exception> void getAndThrow(final Supplier<X> supplier) throws X {
+        throw supplier.get();
+    }
+
+    private static <T extends Throwable> Predicate<T> equalsMessage(final String message) {
+        return subject -> message.equals(subject.getMessage());
+    }
+
+    @Test
+    public final void throwMapped() {
+        final Supplier<RuntimeException> newSubExceptionA = () -> new RuntimeException("SubExceptionA");
+        final Supplier<RuntimeException> newSubExceptionB = () -> new RuntimeException("SubExceptionB");
+        final Supplier<RuntimeException> newSubExceptionC = () -> new RuntimeException("SubExceptionC");
+        final Supplier<RuntimeException> newIOException = () -> new RuntimeException("IOException");
+        final List<Supplier<RuntimeException>> newExceptionList = Arrays.asList(
+                newSubExceptionA, newSubExceptionB, newSubExceptionC, newIOException);
+        for (final Supplier<RuntimeException> newException : newExceptionList) {
+            try {
+                try {
+                    getAndThrow(newException);
+                    fail("should not happen");
+                } catch (final RuntimeException caught) {
+                    Handling.of(caught)
+                            .throwMapped(subject -> Optional.of(subject)
+                                                            .filter(equalsMessage("SubExceptionA"))
+                                                            .map(SubExceptionA::new)
+                                                            .orElse(null))
+                            .throwMapped(subject -> Optional.of(subject)
+                                                            .filter(equalsMessage("SubExceptionB"))
+                                                            .map(SubExceptionB::new)
+                                                            .orElse(null))
+                            .throwMapped(subject -> Optional.of(subject)
+                                                            .filter(equalsMessage("SubExceptionC"))
+                                                            .map(SubExceptionC::new)
+                                                            .orElse(null));
+                    assertSame(newIOException, newException);
+                }
+            } catch (final SubExceptionA caught) {
+                assertSame(newSubExceptionA, newException);
+            } catch (final SubExceptionB caught) {
+                assertSame(newSubExceptionB, newException);
+            } catch (final SubExceptionC caught) {
+                assertSame(newSubExceptionC, newException);
+            }
+        }
+    }
+
+    @Test
+    public final void reThrowIf() {
+        final Supplier<Exception> newSubExceptionA = SubExceptionA::new;
+        final Supplier<Exception> newSubExceptionB = SubExceptionB::new;
+        final Supplier<Exception> newSubExceptionC = SubExceptionC::new;
+        final Supplier<Exception> newIOException = IOException::new;
+        final List<Supplier<Exception>> newExceptionList = Arrays.asList(
+                newSubExceptionA, newSubExceptionB, newSubExceptionC, newIOException);
+        for (final Supplier<Exception> newException : newExceptionList) {
+            try {
+                try {
+                    getAndThrow(newException);
+                    fail("should not happen");
+                } catch (final Exception caught) {
+                    Handling.of(caught)
+                            .reThrowIf(SubExceptionA.class)
+                            .reThrowIf(SubExceptionB.class)
+                            .reThrowIf(SubExceptionC.class);
+                    assertSame(newIOException, newException);
+                }
+            } catch (final SubExceptionA caught) {
+                assertSame(newSubExceptionA, newException);
+            } catch (final SubExceptionB caught) {
+                assertSame(newSubExceptionB, newException);
+            } catch (final SubExceptionC caught) {
+                assertSame(newSubExceptionC, newException);
+            }
+        }
+    }
+
+    @Test
+    public final void throwMappedCause() {
+        final Supplier<RuntimeException> subExceptionA = () -> new RuntimeException(new IOException("SubExceptionA"));
+        final Supplier<RuntimeException> subExceptionB = () -> new RuntimeException(new IOException("SubExceptionB"));
+        final Supplier<RuntimeException> subExceptionC = () -> new RuntimeException(new IOException("SubExceptionC"));
+        final Supplier<RuntimeException> ioException = () -> new RuntimeException(new IOException());
+        final List<Supplier<RuntimeException>> newExceptionList = Arrays.asList(
+                subExceptionA, subExceptionB, subExceptionC, ioException);
+        for (final Supplier<RuntimeException> newException : newExceptionList) {
+            try {
+                try {
+                    getAndThrow(newException);
+                    fail("should not happen");
+                } catch (final RuntimeException caught) {
+                    Handling.of(caught)
+                            .throwMappedCause(cause -> Optional.of(cause)
+                                                               .filter(equalsMessage("SubExceptionA"))
+                                                               .map(SubExceptionA::new)
+                                                               .orElse(null))
+                            .throwMappedCause(cause -> Optional.of(cause)
+                                                               .filter(equalsMessage("SubExceptionB"))
+                                                               .map(SubExceptionB::new)
+                                                               .orElse(null))
+                            .throwMappedCause(cause -> Optional.of(cause)
+                                                               .filter(equalsMessage("SubExceptionC"))
+                                                               .map(SubExceptionC::new)
+                                                               .orElse(null));
+                    assertSame(ioException, newException);
+                }
+            } catch (final SubExceptionA caught) {
+                assertSame(subExceptionA, newException);
+            } catch (final SubExceptionB caught) {
+                assertSame(subExceptionB, newException);
+            } catch (final SubExceptionC caught) {
+                assertSame(subExceptionC, newException);
+            }
+        }
+    }
+
+    @Test
+    public final void reThrowCauseIf() {
+        final Supplier<RuntimeException> newSubExceptionA = () -> new RuntimeException(new SubExceptionA());
+        final Supplier<RuntimeException> newSubExceptionB = () -> new RuntimeException(new SubExceptionB());
+        final Supplier<RuntimeException> newSubExceptionC = () -> new RuntimeException(new SubExceptionC());
+        final Supplier<RuntimeException> newIOException = () -> new RuntimeException(new IOException());
+        final List<Supplier<RuntimeException>> newExceptionList = Arrays.asList(
+                newSubExceptionA, newSubExceptionB, newSubExceptionC, newIOException);
+        for (final Supplier<RuntimeException> newException : newExceptionList) {
+            try {
+                try {
+                    getAndThrow(newException);
+                    fail("should not happen");
+                } catch (final RuntimeException caught) {
+                    Handling.of(caught)
+                            .reThrowCauseIf(SubExceptionA.class)
+                            .reThrowCauseIf(SubExceptionB.class)
+                            .reThrowCauseIf(SubExceptionC.class);
+                    assertSame(newIOException, newException);
+                }
+            } catch (final SubExceptionA caught) {
+                assertSame(newSubExceptionA, newException);
+            } catch (final SubExceptionB caught) {
+                assertSame(newSubExceptionB, newException);
+            } catch (final SubExceptionC caught) {
+                assertSame(newSubExceptionC, newException);
+            }
+        }
+    }
+
+    static class SuperException extends Exception {
+        SuperException() {
+        }
+
+        SuperException(final Throwable cause) {
+            super(cause);
+        }
+    }
+
+    static class SubExceptionA extends SuperException {
+        SubExceptionA() {
+        }
+
+        SubExceptionA(final Throwable cause) {
+            super(cause);
+        }
+    }
+
+    static class SubExceptionB extends SuperException {
+        SubExceptionB() {
+        }
+
+        SubExceptionB(final Throwable cause) {
+            super(cause);
+        }
+    }
+
+    static class SubExceptionC extends SuperException {
+        SubExceptionC() {
+        }
+
+        SubExceptionC(final Throwable cause) {
+            super(cause);
+        }
+    }
+}
