fc38d9a2376574a1c3f1977a06ba3de75cbb96df
andi <akk-pg@team33.de>
2021-03-29 17:47:54 +0200

    enhanced WrappedException

--- src/main/java/de/team33/libs/exceptional/v4/WrappedException.java
+++ src/main/java/de/team33/libs/exceptional/v4/WrappedException.java
@@ -1,21 +1,39 @@
 package de.team33.libs.exceptional.v4;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * An unchecked exception dedicated to wrap checked exceptions.
  */
 public class WrappedException extends RuntimeException {
 
+    private static final String MISSING_CAUSE = "A " +
+            WrappedException.class.getSimpleName() +
+            " is expected to have a real cause but was <null>";
+
     /**
      * Initializes a new instance with the given message and cause.
      */
     public WrappedException(final String message, final Throwable cause) {
-        super(message, cause);
+        this(requireNonNull(cause, MISSING_CAUSE), message);
     }
 
     /**
      * Initializes a new instance with the given cause and its {@link Throwable#getMessage() message}.
      */
     public WrappedException(final Throwable cause) {
-        super(cause.getMessage(), cause);
+        this(requireNonNull(cause, MISSING_CAUSE), null);
+    }
+
+    private WrappedException(final Throwable cause, final String message) {
+        super(Optional.ofNullable(message)
+                      .orElseGet(() -> stdMessage(cause)), cause);
+    }
+
+    private static String stdMessage(final Throwable cause) {
+        return "wrapped: " + cause.getMessage();
     }
 }
